a4166601db1b3377a9b78826eff07d452dfc0a2c
bidwi <billy2perakhoso@gmail.com>
2024-06-13 19:10:29 +0700

    test env change host

--- src/server-infoBenah.js
+++ src/server-infoBenah.js
@@ -18,7 +18,7 @@ const validateApiKey = (request, h) => {
 const init = async () => {
   const server = Hapi.server({
     port: process.env.PORT || 8000,
-    host: process.env.HOST || 'localhost',
+    host: process.env.HOST || '0.0.0.0',
     routes: {
       cors: {
         origin: ['*'],
