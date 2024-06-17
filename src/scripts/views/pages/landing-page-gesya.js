43c0f2f9f893d591c418668102b185919dd4de75
bidwi <billy2perakhoso@gmail.com>
2024-06-17 23:31:09 +0700

    landing page sudah, tinggal pembenah

--- src/scripts/views/pages/landing-page-gesya.js
+++ src/scripts/views/pages/landing-page-gesya.js
@@ -19,14 +19,14 @@ const LandingPageGesya = {
         </div>
     </header>
 
-  <div class="strength-container m-5" style="height:80vh;">
+  <div class="strength-container m-5" style=" overflow: hidden; margin-bottom: 10rem;">
     <div class="title-container p-4">
         <h1 class=" title-text fw-bold text-center fs-1" style="color:#4D869C;">Keunggulan Kami</h1>
     </div>
     <div class="card-container d-flex flex-wrap">
       <div class="row row-cols-1 row-cols-md-4 g-4">
         <div class="col">
-          <div class="card border-0 shadow-lg rounded-4 h-100">
+          <div class="card border-0 shadow-lg rounded-2 h-100">
             <div class="image-container rounded-circle overflow-hidden mx-auto my-3" style="width: 150px; height: 150px; background-color:#4D869C;">
               <img data-src="../../images/deadline.png" class="card-img-top w-100 h-100 lazyload" alt="gambar-waktu" style="object-fit: cover;">
             </div>
@@ -37,7 +37,7 @@ const LandingPageGesya = {
           </div>  
         </div>
         <div class="col">
-          <div class="card border-0 shadow-lg rounded-4 h-100">
+          <div class="card border-0 shadow-lg rounded-2 h-100">
             <div class="image-container rounded-circle overflow-hidden mx-auto my-3" style="width: 150px; height: 150px; background-color:#4D869C;">
               <img data-src="../../images/collaboration.png" class="card-img-top w-100 h-100 p-3 lazyload" alt="gambar-waktu" style="object-fit: cover;">
             </div>
@@ -48,7 +48,7 @@ const LandingPageGesya = {
           </div>  
         </div>
         <div class="col">
-          <div class="card border-0 shadow-lg rounded-4 h-100">
+          <div class="card border-0 shadow-lg rounded-2 h-100">
             <div class="image-container rounded-circle overflow-hidden mx-auto my-3" style="width: 150px; height: 150px; background-color:#4D869C;">
               <img data-src="../../images/real-time.png" class="card-img-top w-100 h-100 p-3 lazyload" alt="gambar-waktu" style="object-fit: cover;">
             </div>
@@ -59,7 +59,7 @@ const LandingPageGesya = {
           </div>  
         </div>
         <div class="col">
-          <div class="card border-0 shadow-lg rounded-4 h-100">
+          <div class="card border-0 shadow-lg rounded-2 h-100">
             <div class="image-container rounded-circle overflow-hidden mx-auto my-3" style="width: 150px; height: 150px; background-color:#4D869C;">
               <img data-src="../../images/quality-of-life.png" class="card-img-top w-100 h-100 p-3 lazyload" alt="gambar-waktu" style="object-fit: cover;">
             </div>
@@ -73,63 +73,67 @@ const LandingPageGesya = {
     </div>
   </div>
 
-  <div class="story-containerer d-flex justify-content-center mx-5 mb-5">
-    <img data-src="../../images/damaged-road-2.jpg" class="img-fluid h-50 w-50 rounded-start-1 shadow-lg mb-5 bg-body-tertiary rounded lazyload" alt="...">
-    <div class="story-content rounded-end-1 p-4" style="background-color:#4D869C">
-      <h2 class="text-light fw-bold fs-1 pb-4 pt-3 ">Cerita Kami</h2>
-      <p class="text-light text-start fw-normal pb-4 fs-5 fst-italic">"Benah Kota lahir dari keprihatinan kami terhadap kondisi lingkungan dan fasilitas umum di kota yang sering kali mengalami kerusakan tanpa perhatian yang memadai. Oleh karena itu, kami menciptakan platform ini untuk memberdayakan masyarakat dalam melaporkan kerusakan fasilitas kota dan meningkatkan kesadaran akan pentingnya menjaga kelestarian lingkungan."</p>
-      <p class="text-light text-start fw-normal fs-5 fst-italic fw-semibold">Gesya Reihan, CEO of Benah Kota</p>
-    </div>
+  <div class="title-container p-4" style="margin-bottom: -3rem;"> 
+      <h1 class=" title-text fw-bold text-center fs-1" style="color:#4D869C; margin-top: 20px;">Apa Kata Mereka?</h1>
   </div>
+
+  <div class="card-body mx-auto mt-5" style="width: 30rem; border-radius: 10px; border: 1.4px solid #ccc;">
+    <div class="card flex-row"><img class="card-img-left example-card-img-responsive w-50 h-50 p-3" src="../../images/damaged-road-2.jpg">
+      <div class="card-body">
+        <h5 class="card-title h5 h4-sm">Saya mampu melapor fasilitas kota yang rusak, seperti jalan, taman kota, atau jembatan.</h5>
+        <h6 class="card-text fw-light">Suparjo, Pengguna Benah Kota</h6>
+      </div>
+    </div>
+   </div>
   
   
 
   <div class="problem-container my-4">
     <div class="title-container p-4"> 
-      <h1 class=" title-text fw-bold text-center fs-1" style="color:#4D869C;">Permasalahan yang Sudah Selesai </h1>
+      <h1 class=" title-text fw-bold text-center fs-1" style="color:#4D869C; margin-top: 60px; margin-bottom: -10px;">Permasalahan yang Sudah Selesai </h1>
     </div>
 
     <div class="row row-cols-1 row-cols-md-4 g-4 p-4">
       <div class="col">
         <div class="card h-100">
-          <img data-src="../../images/damaged-road-2.jpg" class="card-img-top lazyload" alt="...">
+          <img data-src="../../images/jalan-bener1.jpeg" class="card-img-top lazyload" alt="...">
           <div class="card-body">
-            <h5 class="card-title" style="color:#4D869C;">Lorem ipsum dolor sit amet</h5>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <h5 class="card-title fw-bolder" style="color:#4D869C;">Perbaikan Jalan Raya</h5>
+            <p class="card-text">Selama 3 tahun jalan selalu bolong karena pelaporan kepada pemerintah masih sulit, sekarang jadi lebih mudah. dengan fitur lapor, pembenah akan lansung segera ke lokasi untuk memperbaiki seluruh permasalahan kota.</p>
           </div>
         </div>
       </div>
       <div class="col">
         <div class="card h-100">
-          <img data-src="../../images/damaged-road-2.jpg" class="card-img-top lazyload" alt="...">
+          <img data-src="../../images/sutet1.jpg" class="card-img-top lazyload" alt="...">
           <div class="card-body">
-            <h5 class="card-title" style="color:#4D869C;">Lorem ipsum dolor sit amet</h5>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <h5 class="card-title fw-bolder" style="color:#4D869C;">Perbaikan Sutet</h5>
+            <p class="card-text">Selama tinggal di kota kecil, sangat amat sulit untuk mendapatkan akses terhadap listrik untuk kebutuhan rumah tangga. Dengan adanya benah kota segala permasalahan bisa terselesaikan.</p>
           </div>
         </div>
       </div>
       <div class="col">
         <div class="card h-100">
-          <img data-src="../../images/damaged-road-2.jpg" class="card-img-top lazyload" alt="...">
+          <img data-src="../../images/selokan1.jpg" class="card-img-top lazyload" alt="...">
           <div class="card-body">
-            <h5 class="card-title" style="color:#4D869C;">Lorem ipsum dolor sit amet</h5>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <h5 class="card-title fw-bolder" style="color:#4D869C;">Pembersihan selokan</h5>
+            <p class="card-text">Dengan adanya website ini, segala permasalahan sekecil apapun bisa teratasi. Cukup lapor, tunggu pembenah datang ke lokasi, masalah beres.</p>
           </div>
         </div>
       </div>
       <div class="col">
         <div class="card h-100">
-          <img data-src="../../images/damaged-road-2.jpg" class="card-img-top lazyload" alt="...">
+          <img data-src="../../images/jembatan1.jpeg" class="card-img-top lazyload" alt="...">
           <div class="card-body">
-            <h5 class="card-title" style="color:#4D869C;">Lorem ipsum dolor sit amet</h5>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <h5 class="card-title fw-bolder" style="color:#4D869C;">Renovasi Jembatan</h5>
+            <p class="card-text">Sebelumnya terasa sangat susah untuk melewati jembatan. Namun karena website ini, segala permasalahan insfrastruktur kota dari masalah kecil sampai dengan yang besar diselesaikan dengan cepat.</p>
           </div>
         </div>
       </div>
     </div>
     
     <div class="detail-problem-container d-flex justify-content-end"> 
-      <a href="#" class="pt-2 pb-2 ps-4 pe-4 fs-6 fw-semibold me-5" style="color:#4D869C; text-decoration:none;">Lihat Selengkapnya <i class="bi bi-arrow-right ps-1"></i></a>
+      <a href="#/beranda" class="pt-2 pb-2 ps-4 pe-4 fs-6 fw-semibold me-5" style="color:#4D869C; text-decoration:none;">Lihat Selengkapnya <i class="bi bi-arrow-right ps-1"></i></a>
     </div>
   </div>
 
@@ -163,7 +167,20 @@ const LandingPageGesya = {
   },
 
   async afterRender() {
-    // afterRender
+    const updateOverflow = () => {
+      if (
+        window.location.hash === '#/landing-page' ||
+        window.location.hash === '#/' ||
+        window.location.pathname === '/'
+      ) {
+        document.body.style.overflowX = 'hidden';
+      } else {
+        document.body.style.overflowX = 'auto';
+      }
+    };
+
+    window.addEventListener('hashchange', updateOverflow);
+    updateOverflow();
   },
 };
 
